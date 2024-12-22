e66c4fba9c9f179f6b34bcb8eed4ffcd8365743c
Swapnali0405 <swapnalibakal@gmail.com>
2024-12-22 16:30:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args) 
+    {
+       int count = 0;
+        
+       int num = 1;
+
+       while (count < 100) {
+           System.out.print(num + " ");
+           num += 2; 
+           count++; 
+       }
+    }
+}
